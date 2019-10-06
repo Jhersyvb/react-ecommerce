db69b38ce92a2584cca83cb323084ed0d82e7f24
Jhersy <jhersyvb@gmail.com>
2019-10-05 19:04:14 -0500

    4 Adding products

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css'
 
@@ -7,9 +7,15 @@ import ProductsList from './components/ProductsList'
 import SingleProduct from './components/SingleProduct'
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  const addProduct = product => {
+    setProducts([...products, product])
+  }
+
   return (
     <Router>
-      <div className="App">
+      <div id="app">
         <aside>
           <Link to={`/`}>Products</Link>
           <Link to={`/add-product`}>Add product</Link>
@@ -17,7 +23,10 @@ function App() {
 
         <main>
           <Route exact path="/" component={ProductsList} />
-          <Route path="/add-product" component={AddProduct} />
+          <Route
+            path="/add-product"
+            render={({ history }) => <AddProduct addProduct={addProduct} history={history} />}
+          />
           <Route path="/product/:slug" component={SingleProduct} />
         </main>
       </div>
--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,7 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AddProduct = () => {
-  return <div>Add product</div>
+const slugify = str => {
+  str = str || ''
+  const a = 'àáäâèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;άαβγδεέζήηθιίϊΐκλμνξοόπρσςτυϋύΰφχψωώ'
+  const b = 'aaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------aavgdeeziitiiiiklmnxooprsstyyyyfhpoo'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return str
+    .toString()
+    .trim()
+    .toLowerCase()
+    .replace(/ου/g, 'ou')
+    .replace(/ευ/g, 'eu')
+    .replace(/θ/g, 'th')
+    .replace(/ψ/g, 'ps')
+    .replace(/\//g, '-')
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special chars
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
+
+const AddProduct = props => {
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [description, setDescription] = useState('')
+  const [image, setImage] = useState('')
+
+  const handleChangeName = e => {
+    setName(e.target.value)
+  }
+
+  const handleChangePrice = e => {
+    setPrice(e.target.value)
+  }
+
+  const handleChangeDescription = e => {
+    setDescription(e.target.value)
+  }
+
+  const handleChangeImage = e => {
+    setImage(e.target.value)
+  }
+
+  const addProduct = e => {
+    e.preventDefault()
+    props.addProduct({ name, price, description, image, slug: slugify(name) })
+    props.history.push('/')
+  }
+
+  return (
+    <div className="section">
+      <form onSubmit={addProduct}>
+        <h1>Add Product</h1>
+        <div className="field">
+          <label>Name:</label>
+          <input required onChange={handleChangeName} />
+        </div>
+        <div className="field">
+          <label>Price in $:</label>
+          <input required onChange={handleChangePrice} />
+        </div>
+        <div className="field">
+          <label>Description:</label>
+          <textarea required onChange={handleChangeDescription} />
+        </div>
+        <div className="field">
+          <label>Image URL:</label>
+          <input required onChange={handleChangeImage} />
+        </div>
+        <input type="submit" value="Add" className="button" />
+      </form>
+    </div>
+  )
 }
 
 export default AddProduct
